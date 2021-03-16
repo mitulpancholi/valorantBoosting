c788f2e8e70f6eab640a708d9cd9703f2b475957
Mitul Pancholi <mitul.pancholi@live.com>
2021-03-17 00:18:07 +0530

    Navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+import backArrow from "../assets/images/home/Union.png"
+
+const NavbarWrapper = styled.div`
+  height: 100px;
+  background-color: rgb(22, 20, 20);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position:relative;
+  top:0;
+  left:0;
+`;
+const NavHead = styled.nav`
+  width: 739px;
+  height: 40px;
+  justify-content: space-between;
+  display: flex;
+  & > a {
+    font-size: 30px;
+    text-decoration: none;
+    color: #B8B9BD;
+  }
+`;
+
+const NavSpan = styled.span`
+margin: 3px 10px;
+color: #B8B9BD;
+font-size:24px;
+`
+
+const BackButton = styled.div`
+position:absolute;
+top:40px;
+left:20px;
+color:white;
+display:flex;
+justify-content:center;
+align-items:center;
+`
+
+const GoBackText = styled.span`
+font-size:16px;
+color:white;
+margin-left:5px;
+`
+const Navbar = () => {
+  return (
+    <>
+      <NavbarWrapper>
+        <NavHead>
+          <a href="/html/">HOME</a>
+          <NavSpan>|</NavSpan>
+          <a href="/css/">VOUCHES</a>
+          <NavSpan>|</NavSpan>
+          <a href="/js/">OUR PLANS</a>
+          <NavSpan>|</NavSpan>
+        <a href="/python/">CONTACT US</a>
+        </NavHead>
+        <BackButton>
+            <div>
+                <img src={backArrow} alt="Back Arrow"></img>
+            </div>
+            <GoBackText>
+                GO BACK
+            </GoBackText>
+        </BackButton>
+      </NavbarWrapper>
+    </>
+  );
+};
+
+export default Navbar;
